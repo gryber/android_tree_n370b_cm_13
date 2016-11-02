5c5f65b0941c56a8f41b2bad8440f88e456a9c7f
gryber <lenava678@gmail.com>
2016-11-02 17:10:38 +0200

    initial commit

---
+++ doogee/n370b/full_n370b.mk
@@ -0,0 +1,25 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from porridge
+$(call inherit-product, device/doogee/n370b/device.mk)
+
+PRODUCT_DEVICE := n370b
+PRODUCT_NAME := full_n370b
+PRODUCT_BRAND := doogee
+PRODUCT_MODEL := n370b
+PRODUCT_MANUFACTURER := doogee
